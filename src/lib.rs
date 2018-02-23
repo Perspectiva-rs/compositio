e64f1125aa4da9c5b81d3b38278533b7b62833a7
Rui Lourenço <ruilourenco96@gmail.com>
2018-02-23 18:04:38 +0000

    Made indexing fully functional; Implemented Matrix structs using Vec!<usize> as D.

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,14 @@ use std::borrow::Borrow;
 use std::borrow::BorrowMut;
 
 pub trait Coord{
-    type Dimension: Dim;
-    fn index(&self, dim: &Self::Dimension) -> Option<usize>;
+    type Dimension: Dim + ?Sized;
+    fn index_checked(&self, dim: &Self::Dimension) -> Option<usize>;
 
 }
 
-impl Coord for [usize;2]{
-    type Dimension = [usize;2];
-    fn index(&self, dim: &[usize;2]) -> Option<usize>{
+impl Coord for [usize]{
+    type Dimension =  [usize];
+    fn index_checked(&self, dim: &[usize]) -> Option<usize>{
         let array = dim.as_array();
         if dim.check_bounds(&self) {
             Some(self[0] + self[1] * array[1])  
@@ -22,16 +22,19 @@ impl Coord for [usize;2]{
 }
 
 pub trait Dim{
-    type Coord: Coord;
+    type Coord: Coord + ?Sized;
     fn as_array(&self) -> &[usize];
     fn as_mut_array(&mut self) -> &mut [usize];
     fn increase_size(&mut self, axis: usize, inc: usize);
     fn get(&self,axis:usize) -> usize;
+    fn set(&mut self,axis:usize,new_dim:usize);
     fn check_bounds(&self, coord: &Self::Coord)->bool;
+    fn is_empty(&self) ->bool;
+    fn is_column_vector(&self) -> bool;
 }
 
-impl Dim for [usize;2] {
-    type Coord = [usize;2];
+impl Dim for [usize] {
+    type Coord = [usize];
     
     fn as_array(&self) -> &[usize]{
         self.borrow()
@@ -46,79 +49,102 @@ impl Dim for [usize;2] {
     fn get(&self,axis:usize) -> usize{
         self[axis]
     }
+    fn set(&mut self,axis:usize,new_dim:usize){
+        self[axis] = new_dim;
+    }
     
     fn check_bounds(&self, coord: &Self::Coord)->bool{
-        if self.get(0) >  coord[0] && self.get(1) >  coord[1]{
+        if self[0] >  coord[0] && self[1]>  coord[1]{
             true
         }else{
             false
         }
     }
+    
+    fn is_empty(&self) ->bool{
+        if &[0usize,0usize] == self {
+            true
+        }else{
+            false
+        }
+    }
+        fn is_column_vector(&self) -> bool{
+            if self[0] > 1 && self[1] == 0{
+                true
+            }else{
+                false
+            }
+        }
 }
 
-#[derive(Debug)]
-pub struct Matrix<T,D:Dim>{
+#[derive(Debug,PartialEq)]
+pub struct Matrix<T,D:  ?Sized>{
     pub data: Vec<T>,
-    pub dim: D
+    pub dim: D,
 }
-impl<T:Sized,D:Dim> Matrix<T,D>{
-    pub fn new(data:Vec<T>,dim:D) -> Matrix<T,D> {
+impl<'a,T> Matrix<T,Vec<usize>>{
+    pub fn new(data:Vec<T>,dim:Vec<usize>) -> Matrix<T,Vec<usize>> {
         Matrix{data , dim}
     }
-    pub fn dim(&self) -> &D {
-        &self.dim
+    pub fn dim(&'a self) -> &[usize] {
+        &self.dim 
     }
-    fn mut_dim(&mut self) -> &mut D {
+    fn mut_dim(& mut self) -> & mut [usize] {
         &mut self.dim
     }
 
     pub fn data(&self) -> &Vec<T>{
-        &self.data
+        &self.data 
     }
     pub fn data_mut(&mut self) -> &mut Vec<T> {
         &mut self.data
     }
 
+   
+   
+
     pub fn append_column(&mut self, column: &mut Vec<T>) {
-        
-        {
-            let size = self.mut_dim().as_mut_array();
-            if size == [0usize,0usize]{
-                size[1] = column.len();
+        {let  dim = self.mut_dim();
+            if !dim.is_empty() {
+                dim[0] = column.len();
             }else{
-                if column.len() != size[1] {
-                    panic!("Matrix columns have variable length");
+                if column.len() != dim[0]{
+                        panic!("Matrix columns have variable length");
                 }
             }
+        dim.increase_size(1,1); 
         }
-
-        self.dim.increase_size(0,1);
         self.data.append(column);
         
     }
-}
-
-impl<T,C,D> Index<C> for Matrix<T,D>
-    where 
-        T: Sized,
-        C: Coord<Dimension = D>,
-        D: Dim
-    {
-
-    type Output = T;    
-    fn index(&self, index: C) -> &T{
-        let data = self.data();
-        &data[index.index( self.dim() ).unwrap()]
+    pub fn append_line(&mut self, line: &mut Vec<T>){
+        unimplemented!();
     }
 }
 
-impl<T:fmt::Display> fmt::Display for Matrix<T,[usize;2]> {
+impl<'a,'b, T> Index<&'a [usize]> for Matrix<T,Vec<usize>>
+    
+
+    {
+    type Output = T;    
+    fn index(& self, index:&[usize]) -> &T{
+        let data = self.data();
+        &data[index.index_checked( self.dim() ).unwrap()]
+    }
+}
+
+impl<'a,T> fmt::Display for Matrix<T,Vec<usize>> 
+    where
+        T:fmt::Display,
+        
+    
+    {
     fn fmt(&self, f: &mut fmt::Formatter ) ->fmt::Result {
         
-        for n in 0..self.dim.get(1) {
-            for m in 0..self.dim.get(0){
+        for n in 0..self.dim()[0] {
+            for m in 0..self.dim()[1]{
                 
-                write!(f,"{} ", self[[n,m]])?;
+                write!(f,"{} ", self[&[n,m]])?;
             }
             write!(f,"\n")?;
        }
@@ -132,15 +158,14 @@ impl<T:fmt::Display> fmt::Display for Matrix<T,[usize;2]> {
 macro_rules! mat {
         ($($($x:expr),*);*) => {
             {
-                let mut matrix = Matrix::new(vec![],[0usize, 0usize]);
+                let mut matrix = Matrix::new(vec![],vec![0, 0]);
                 $(let mut vector = vec![$($x),*];
                 matrix.append_column(&mut vector);
                 );*
-                println!("{}",matrix );
+
                 matrix
             }
         };
-        
     }
 
 #[macro_use]
@@ -154,28 +179,30 @@ mod tests {
     //use super::macros;
     #[test]
     fn vector() {
-        let matrix = Matrix::new(vec![2,3,4],[1 as usize, 3 as usize]);
+        let matrix = Matrix::new(vec![3,3,4],vec![3 as usize, 1 as usize]);
         let macro_matrix = mat![2,3,4];
-        println!("{}",macro_matrix );
+        println!("\n{}",matrix);
+        println!("{},{}",matrix[&[1,0]],matrix[&[2,0]]);
+        
 
-        assert_eq!(matrix.data, macro_matrix.data);
+        assert_eq!(matrix, macro_matrix);
         //assert_eq!(matrix.dim(), macro_matrix.dim());
 
     }
     #[test]
     fn matrix() {
-        let matrix = Matrix::new(vec![2,3,4,5,6,7,8,9,10],[3 as usize, 3 as usize]);
+        let matrix = Matrix::new(vec![2,3,4,5,6,7,8,9,10],vec![3 as usize, 3 as usize]);
         let macro_matrix = mat![2,3,4;5,6,7;8,9,10];
         
-        assert_eq!(matrix.dim(), macro_matrix.dim());
+        assert_eq!(matrix, macro_matrix);
        // assert_eq!(matrix.data, macro_matrix.data);
 
     }
     #[test]
     fn indexing() {
         let matrix = mat![2,3,4;5,6,7;8,9,10];
-        println!("{}",matrix );
+        println!("{}",matrix);
         
-        assert_eq!(5, matrix[[1,0]]);
+        assert_eq!(5, matrix[&[1,0]]);
     }
 }
